ac42be60a441f8a16589f8f0a04824a1e92e739b
EliorRonen <Ronenelior2014@gmail.com>
2024-09-09 18:40:04 +0300

    python

---
+++ operators part 3.py
@@ -0,0 +1,16 @@
+e = 1000000000
+y = 1000000000
+if e is y:
+    print("i'm buzzila")
+else:
+    print("obbay the zilla!!!!!!")
+if e is not y:
+    print("sigma")
+else:
+    print("skibidi")
+    e = 100000000000000000
+    y = 100000000000000000
+if type(e) is str:
+    print("success")
+else:
+    print("fail")
