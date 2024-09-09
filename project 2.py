7cc44f9493ee23767008af22377305739344ca73
EliorRonen <Ronenelior2014@gmail.com>
2024-09-09 15:49:11 +0300

    python

---
+++ project 2.py
@@ -0,0 +1,10 @@
+#the welcoming
+print("welcome to my project sneha")
+#my age in 3 days
+print("10")
+#my favorite foods
+print("pizza,steak and everything else")
+#my favorite video game
+print("roblox,dragon ball legends,fornite")
+#an integer and a string
+print("python",10)
