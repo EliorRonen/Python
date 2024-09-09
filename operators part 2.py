31b9cc5093c9515154d0bcd3c0c9df6edbc62c84
EliorRonen <Ronenelior2014@gmail.com>
2024-09-09 15:48:39 +0300

    initial commit

---
+++ operators part 2.py
@@ -0,0 +1,12 @@
+a = 10
+b = 9
+c = 10
+if a and b and c:
+    print("there all together, that's happy")
+else:
+    print("there not all together, why is that happening? i don't know")
+
+if a or b or c:
+    print("everybody is diffrent")
+else:
+    print("there's nobody special in town, that is not weird")
