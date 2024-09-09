edc325ac96ed6c15d34212f483a36579842534db
EliorRonen <Ronenelior2014@gmail.com>
2024-09-09 19:00:27 +0300

    python

---
+++ Bitwise.py
@@ -0,0 +1,10 @@
+e = "i like fruits"
+if "fruits" in e:
+    print("i like fruits")
+else:
+    print("why dosen't it say i like fruits!!!!!")
+e = (10,1)
+if 10 not in e:
+    print("in only there days im 10 years old")
+else:
+    print("im 10 already[that's not true]")
