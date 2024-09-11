6d31f759a821ede635aa22c9ef8407d512536cb5
EliorRonen <Ronenelior2014@gmail.com>
2024-09-11 18:56:37 +0300

    division

---
+++ operator precedence part2.py
@@ -0,0 +1,8 @@
+numn = int(input("please enter the numerator:"
+))
+numd = int(input("please enter the denominator:"
+))
+if numn%numd == 0:
+    print("it worked")
+else:
+    print("it didin't work")
