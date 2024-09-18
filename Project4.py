21c8514b5a1d68d0ef8cee811c142ab4a4f30d1f
EliorRonen <Ronenelior2014@gmail.com>
2024-09-18 15:29:06 +0300

    Project4.py

---
+++ Project4.py
@@ -0,0 +1,11 @@
+Elior1 = 154
+Elior2 = 155
+Elior3 = 156
+Elior4 = 157
+Elior5 = 158
+
+answer = Elior1+Elior2+Elior3+Elior4+Elior5
+print("the answer of all 5 Eliors is : ",answer)
+
+average = answer/5
+print("the average of the answer is: ",average)
