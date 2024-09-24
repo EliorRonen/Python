728a65d825886ca1f1828797d38684465678b2f6
EliorRonen <Ronenelior2014@gmail.com>
2024-09-24 19:48:42 +0300

    identfy operators

---
+++ Project 7.py
@@ -0,0 +1,13 @@
+e = 10
+l = 10
+i = 9
+if e is l:
+    print("There the same number")
+else:
+    print("There not the same number")
+
+
+if e is not i:
+    print("There not the same number")
+else:
+    print("There the same number")
