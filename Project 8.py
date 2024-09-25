c2db5cb6639b08a5f3b79de9818dc33d8c662085
EliorRonen <Ronenelior2014@gmail.com>
2024-09-25 17:32:09 +0300

    Project 8

---
+++ Project 8.py
@@ -0,0 +1,14 @@
+e = 10
+l = 9
+i = 8
+o = 7
+r = 6
+print((e+l) * i / o * r)
+
+name = "Elior"
+age = 10
+
+if name == "Elior" == age >= 10:
+    print("Elior is 10 years old")
+else:
+    print("Elior is younger than 10 years old")
