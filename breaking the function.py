76929e7be0aee786a8bf61d8488d465deeac57a5
EliorRonen <Ronenelior2014@gmail.com>
2024-10-06 18:25:07 +0300

    i almost submited this project when the code wasen't finished

---
+++ breaking the function.py
@@ -0,0 +1,8 @@
+e = input("Please enter your items in the e value:")
+
+for i in e:
+    if i == "e":
+        print("e is found")
+        break
+    else:
+        print("the letter e is not found")
