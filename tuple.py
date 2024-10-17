c6d96e1172662a5e6bb1708f2cf792f225263348
EliorRonen <Ronenelior2014@gmail.com>
2024-10-17 19:41:51 +0300

    slicing the values instead of the bread

---
+++ tuple.py
@@ -0,0 +1,24 @@
+KSI_has_a_cringe_song = ('yay',True,10.0,10)
+print(KSI_has_a_cringe_song[1])
+
+
+
+
+
+
+
+ksi_has_a_cringe_song = (1,2,3,4,5,10)
+ksi_has_a_cringe_song = ksi_has_a_cringe_song + (6,7,8,9,10)
+print(ksi_has_a_cringe_song)
+print(ksi_has_a_cringe_song.count(10))
+
+
+
+
+
+
+Thick_of_it_everybody_knows = (2,4,6,8,10,12,14,16,18,20)
+slice = Thick_of_it_everybody_knows[2:10]
+print(slice)
+slice = Thick_of_it_everybody_knows[:10]
+print(slice)
