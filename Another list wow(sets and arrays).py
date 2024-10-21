6c68fa670ac71665f45c342ea0163518b962971d
EliorRonen <Ronenelior2014@gmail.com>
2024-10-21 18:39:32 +0300

    share and submit

---
+++ Another list wow(sets and arrays).py
@@ -0,0 +1,10 @@
+set1 = {1,2,3}
+set2 = {'toji',10.0,10,True}
+print(set1,set2)
+set3 = {1,2,3,4,3,2,4}
+print(set3)
+set_of_list = set([1,2,3,4,5,6,7,8,9,10])
+print(set_of_list)
+Final_set = {0,2,4,6,8,10}
+Final_set.pop()
+print(Final_set)
