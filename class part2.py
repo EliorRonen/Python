b40a40ba25195388012f1d97791a70a68b4e2abe
EliorRonen <Ronenelior2014@gmail.com>
2024-10-30 17:57:20 +0200

    vehicles

---
+++ class part2.py
@@ -0,0 +1,11 @@
+class vehicle:
+
+    def __init__(self,maxspeed,myleage):
+        self.maxspeed = maxspeed
+        self.myleage = myleage
+
+
+ob = vehicle(240,100)
+
+print("the maxspeed of the vechile is:",ob.maxspeed)
+print("your leage of the vechile is:",ob.myleage)
