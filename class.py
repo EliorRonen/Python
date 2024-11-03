708a5ce267466421154c0ea4d9d221759d0d1848
EliorRonen <Ronenelior2014@gmail.com>
2024-11-03 17:53:08 +0200

    class part 2.py

---
+++ class.py
@@ -0,0 +1,14 @@
+class string:
+
+    def __init__(self):
+        self.str1 = ""
+        skibidi = input("Enter the value please:")
+    def skibidi(self):
+        self.str1 = input("enter the string:")
+    def printing_the_string(self):
+        print("the string is:",self.str1)
+        
+        
+str1 = string()
+str1.skibidi()
+str1.printing_the_string()
---
+++ class2.py
@@ -0,0 +1,12 @@
+class employee:
+
+    def __init__(self):
+        print("Employee created")
+    def __del__(self):
+        print("Employee Deleted")
+    def create_ob():
+        ob = employee()
+        return(ob)
+    
+ob = employee()
+print("create_ob")
