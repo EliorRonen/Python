6d9ff7d549a3cbd6bbb2ab1630ff7e19d33b4c3f
EliorRonen <Ronenelior2014@gmail.com>
2024-11-03 17:05:59 +0200

    the return function

---
+++ project 17.py
@@ -0,0 +1,7 @@
+def customer_due(the_bill,the_amount):
+    the_bill = 30
+    the_amount = 10
+    return(the_bill+the_amount)
+
+Customer_Due_After_Caculations = (customer_due(the_bill="30",the_amount="10"))
+print("the customer due is:",Customer_Due_After_Caculations)
